e2e8074957604aae53562cb1fecb743baac7838f
sus <iamsuchabozo12345@gmail.com>
2025-11-26 20:17:41 -0800

    oop ig need more stuff for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const repository = 'text-adventure-thing';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const base = isProduction ? `/${repository}/` : '';
+
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		outDir: 'dist', // GitHub Pages-friendly
+	},
+	base: base,
 });
