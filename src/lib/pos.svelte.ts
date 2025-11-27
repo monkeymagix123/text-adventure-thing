09f1999eac48b9ad7a7ea0b745e00ac71d867db4
sus <iamsuchabozo12345@gmail.com>
2025-11-26 19:38:30 -0800

    add more content

--- src/lib/pos.svelte.ts
+++ src/lib/pos.svelte.ts
@@ -1,10 +1,13 @@
 import { State } from "./state";
 
 const home = new State("Home", "Welcome to the game");
-const start = new State("[Background]", "Start the game");
+const start = new State("Start", "You find yourself in a dark room. There's not quite anything to do at the moment, so you might as well explore.");
 home.addOption("Play", start);
 
-start.addOption("[a]", start);
+start.addOption("Explore", start);
+start.addOption("Look around", start);
+start.addOption("Sit down", start);
+start.addOption("Exit game", home); // style this diff?
 
 let state = $state(home);
 
