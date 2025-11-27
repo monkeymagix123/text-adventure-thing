524257e2d17c3e70873a79d37274f79406fb06a9
sus <iamsuchabozo12345@gmail.com>
2025-11-26 19:17:45 -0800

    separate game component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,10 @@
 <script>
 	import { getState } from "$lib/pos.svelte";
-	import Button from "../components/Button.svelte";
+	import Game from "../components/Game.svelte";
 
     const state = $derived(getState());
 </script>
 
 <main>
-    <h2> {state.title} </h2>
-    <p> {state.description} </p>
-
-    {#each state.options as option}
-        <Button onclick={ () => { console.log(option); state.doOption(option); } }> {option.action} </Button>
-    {/each}
-
-    <button> test </button>
-
-    <!-- <style>
-        h1 {
-            color: #ff3e00;
-            text-transform: uppercase;
-            font-size: 4em;
-            font-weight: 100;
-        }
-    </style> -->
+    <Game></Game>
 </main>
