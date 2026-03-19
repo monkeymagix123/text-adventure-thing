7d93e014c255e139059398eeeadc3fcaf842c52d
sus <iamsuchabozo12345@gmail.com>
2026-03-18 18:29:56 -0700

    add descriptions to flag

--- src/lib/flags.ts
+++ src/lib/flags.ts
@@ -2,13 +2,18 @@ import content from "$lib/content.json";
 
 // const localStorage = window.localStorage;
 
+// FLAGS
+/** All flags, including session flags */
 const flags: Record<string, boolean> = {};
+/** List of session flags */
 const tempFlags: Array<string> = [];
 
+/** Set non-session flags to false */
 for (const flag of content.flags) {
     flags[flag] = false;
 }
 
+/** Set session flags to false and stores them as session flags */
 for (const flag of content.sessionFlags) {
     flags[flag] = false;
     tempFlags.push(flag);
@@ -16,20 +21,28 @@ for (const flag of content.sessionFlags) {
 
 export default flags;
 
+/** Sets flag to value */
 export function setFlag(flag: string, value: boolean) {
     flags[flag] = value;
 
     // maybe show something in achievements?
 }
 
+/** Checks if all flags in 'reqs' are true */
 export function hasAllFlags(reqs: string[]): boolean {
     return reqs.every((flag: string) => { return flags[flag] });
 }
 
+/** Checks if all flags in 'reqs' are false */
 export function hasNoFlags(reqs: string[]): boolean {
     return reqs.every((flag: string) => { return !flags[flag] });
 }
 
+/**
+ * Resets certain flags to false.
+ * @param tempOnly If true, resets only session flags.  
+ * Otherwise, resets all flags.
+ */
 export function resetFlags(tempOnly: boolean = true) {
     for (const flag of Object.keys(flags)) {
         if (!tempOnly || tempFlags.includes(flag)) {
